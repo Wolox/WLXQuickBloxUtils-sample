e5b1fbe3c3f897344a5b581f222731817fb9784c
Damián Finkelstein <dfinkelstein@wolox.com.ar>
2015-07-10 14:30:00 -0300

    files organized in folders

---
+++ WLXQuickBloxUtils-sample/WLXQuickBloxUtils-sample/ViewControllers/LoginViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WLXQuickBloxUtils-sample
+//
+//  Created by Damian on 7/10/15.
+//  Copyright (c) 2015 Wolox. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
