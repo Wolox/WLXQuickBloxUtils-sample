047ac9ffcfdbb749b08f1c536ecf16b13ba6bd56
Damián Finkelstein <dfinkelstein@wolox.com.ar>
2015-07-23 19:30:23 -0300

    view models added

---
+++ WLXQuickBloxUtils-sample/WLXQuickBloxUtils-sample/Storyboards/ViewModels/UserViewModel.h
@@ -0,0 +1,16 @@
+//
+//  UserViewModel.h
+//  WLXQuickBloxUtils-sample
+//
+//  Created by Damian on 7/10/15.
+//  Copyright (c) 2015 Wolox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "WLXQuickBloxUtils.h"
+
+@interface UserViewModel : NSObject
+
+- (instancetype)initWithQBUUser:(QBUUser *)user;
+
+@end
