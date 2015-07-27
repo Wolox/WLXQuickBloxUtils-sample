b56a9f9133bd6a9ff764ea4862afc18e15387150
Damián Finkelstein <dfinkelstein@wolox.com.ar>
2015-07-27 13:51:59 -0300

    login and user view models

---
+++ WLXQuickBloxUtils-sample/WLXQuickBloxUtils-sample/Foundation categories/NSString+CapitalizeFirstWord.h
@@ -0,0 +1,15 @@
+//
+//  NSString+capitalizeFirstWord.h
+//  ToBuy
+//
+//  Created by Damian on 1/5/15.
+//  Copyright (c) 2015 wolox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (CapitalizeFirstWord)
+
+- (NSString*)CapitalizeFirstWord;
+
+@end
